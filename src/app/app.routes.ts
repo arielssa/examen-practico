bc707bc769ca617859f15865f1658c02be03da61
arielssa <arielss77cc@gmail.com>
2025-11-26 18:00:56 -0500

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AcercaComponent } from './acerca/acerca.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'acerca', component: AcercaComponent },
+    ]
+  }
+];
